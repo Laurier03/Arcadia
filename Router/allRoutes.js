624a5bcfa31a3267c918f6a8e9d07a4a07b0fab7
Laurentiu <laurentiubudacpro@gmail.com>
2024-07-09 12:43:03 +0200

    Install Bootstrap

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,6 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/Contact", "Contact","/pages/contact.html", "/js/contact.js" )
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
