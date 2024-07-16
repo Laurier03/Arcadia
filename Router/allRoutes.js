0474e0c91e244a2136a6c4fc48147616937c80e2
Laurentiu <laurentiubudacpro@gmail.com>
2024-07-16 15:08:35 +0200

    Ajout de la page des habitats

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,6 +3,10 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/habitats", "Nos Habitats", "/pages/habitats.html"),
+    new Route("/contact", "Contact", "/pages/contact.html"),
+    new Route("/services", "Nos Services", "/pages/services.html"),
+    new Route("/connexion", "Connexion", "/pages/connexion.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
