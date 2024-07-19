fe8f46a8a52d4207c9e2b404db0b7f1e518d329d
Laurentiu <laurentiubudacpro@gmail.com>
2024-07-19 13:34:56 +0200

    Création du Dashboard administrateur

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,8 +6,8 @@ export const allRoutes = [
     new Route("/habitats", "Nos Habitats", "/pages/habitats.html", []),
     new Route("/contact", "Contact", "/pages/contact.html", []),
     new Route("/services", "Nos Services", "/pages/services.html", []),
-    new Route("/connexion", "Connexion", "/pages/connexion.html", "/js/auth/connexion.js", ["disconnected"]),
-    new Route("/admin", "Admin", "/pages/admin.html", "/js/auth/admin.js", ["admin"]),
+    new Route("/connexion", "Connexion", "/pages/connexion.html", ["disconnected"], "/js/auth/connexion.js"),
+    new Route("/admin", "Admin", "/pages/admin.html", ["admin"], "/js/auth/admin.js"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
